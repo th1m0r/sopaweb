4f7471db4bb7c52f2c0d7ebc669303758fc94c6b
Thiago Moreno <thiagocmoreno@gmail.com>
2019-09-23 20:12:51 -0300

    configuracao da api para o heroku

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    baseURL: "http://192.168.0.19:6367/sopa/api",
+    baseURL: "https://apisopa.herokuapp.com/sopa/api",
 });
 
 export default api;
