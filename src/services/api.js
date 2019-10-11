d4f572a55578a6e608456e2314b3fd2356856b9f
Thiago Moreno <thiagocmoreno@gmail.com>
2019-10-11 15:16:28 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const REMOTO = "177.89.50.127:8080/sopa/api"
+const REMOTO = "http://177.89.50.127:8080/sopa/api"
 const api = axios.create({
     baseURL: REMOTO,
 });
