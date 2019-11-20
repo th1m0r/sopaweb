a98ce927815b60c918af453a317686b4be374a55
Thiago Moreno <thiagocmoreno@gmail.com>
2019-11-20 14:46:34 -0300

    correcao das configuracoes do grafico

--- src/components/GraficoFrequencia/index.js
+++ src/components/GraficoFrequencia/index.js
@@ -4,7 +4,7 @@ import { HorizontalBar, Bar } from 'react-chartjs-2'
 export default function GraficoFrequencia() {
 
     const [resumoMensal, setResumoMensal] = useState({
-        labels: ['07-11-2019', '14-11-2019'],
+        labels: ['07-11-2019', '14-11-2019','21-11-2019', '28-11-2019'],
         datasets: [
             {
                 label: 'Ponto 1',
@@ -13,16 +13,16 @@ export default function GraficoFrequencia() {
                 borderWidth: 1,
                 hoverBackgroundColor: 'rgba(60,141,188,0.4)',
                 hoverBorderColor: 'rgba(60,141,188,1)',
-                data: [14, 12]
+                data: [14,12,0,0]
             },
             {
                 label: 'Ponto 2',
-                backgroundColor: 'rgba(210, 214, 222, 1)',
-                borderColor: 'rgba(210, 214, 222, 1)',
+                backgroundColor: 'rgba(21, 214, 90, 1)',
+                borderColor: 'rgba(21, 214, 90, 1)',
                 borderWidth: 1,
-                hoverBackgroundColor: 'rgba(220,220,220,0.4)',
-                hoverBorderColor: 'rgba(220,220,220,1)',
-                data: [31, 23]
+                hoverBackgroundColor: 'rgba(21,220,90,0.4)',
+                hoverBorderColor: 'rgba(21,220,19,1)',
+                data: [31,23,0,0]
             },
             {
                 label: 'Ponto 3',
@@ -31,7 +31,7 @@ export default function GraficoFrequencia() {
                 borderWidth: 1,
                 hoverBackgroundColor: 'rgba(230,130,92,0.4)',
                 hoverBorderColor: 'rgba(230,130,92,1)',
-                data: [12, 13]
+                data: [12,13,0,0]
             },
         ]
     })
@@ -52,12 +52,22 @@ export default function GraficoFrequencia() {
                 </div>
                 <div className="box-body">
                     <div className="chart">
-                        <HorizontalBar
+                        <Bar
                             data={resumoMensal}
-                            height={300}
+                            height={230}
+                            width={230}
                             options={{
-                                barThickness: 3,
                                 maintainAspectRatio: false,
+                                scales: {
+                                    yAxes: [{
+                                        ticks: {
+                                            beginAtZero: true
+                                        }
+                                    }],
+                                    xAxes: [{
+                                        barPercentage: 0.9
+                                    }]
+                                }
                             }}
                         />
                     </div>
